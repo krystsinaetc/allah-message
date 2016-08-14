7a52a45c08e91520205dd5d9e9fc04223bbf628f
Kristina <isitiriss@gmail.com>
2016-08-15 00:57:44 +0300

    final version

--- ddos.py
+++ ddos.py
@@ -2,27 +2,26 @@
 
 import hexchat
 import random
+import getpass
 
 __module_name__ = "Ddos simulator"
 __module_author__ = "KristinaEtc"
 __module_version__ = "0.1"
 __module_description__ = "Ddos simulator"
 
-world = ['sun','moon','stars','planets','galaxies','oceans','mountains','trees','mom','dad','boss','job',
-'dollar','degree''medicine','customers','nobody','light','fan','businessess']
+
 
 def send_stuff(word, word_eol, userdata):
-    my_msg = "%s are not doing Allah is doing" %(random.choice(world))
+    curr_user = getpass.getuser()
+    fname = '/home/' + curr_user + '/.config/hexchat/addons/allah.txt'
+    with open(fname) as f:
+        content = f.readlines()
 
     channel = hexchat.get_info("channel")
-    if len(word) < 2: #print message to a channel
-        hexchat.command("MSG %s %s" %(channel, my_msg))
-    elif len(word) == 2: #print private message to a user
-        hexchat.command("MSG %s %s" %(word[1], my_msg))
-    elif len(word) == 3: #ddos version for boorish children
-        for x in range(0, 10):
-            my_msg = "%s are not doing Allah is doing" %(random.choice(world))
-            hexchat.command("MSG %s %s" %(word[1], my_msg))
+    if len(word) == 2: #ddos version for boorish children
+        for x in range(0, 1000):
+            msg = random.choice(content)
+            hexchat.command("MSG %s %s" %(word[1], msg))
 
 hexchat.hook_command("ddos", send_stuff)
 
