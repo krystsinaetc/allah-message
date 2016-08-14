3666813b00a2fa2ceab22718e25107bc23a30b09
Kristina <isitiriss@gmail.com>
2016-08-14 23:58:50 +0300

    changed message

--- allah.py
+++ allah.py
@@ -8,16 +8,19 @@ __module_author__ = "KristinaEtc"
 __module_version__ = "0.1"
 __module_description__ = "Allah stuff for user"
 
-world = ['Allah1', 'Allah2', 'Allah3', 'Allah4', 'Allah5', 'Allah6', 'Allah7', 'Allah8', 'Allah9']
-
+world = ['sun','moon','stars','planets','galaxies','oceans','mountains','trees','mom','dad','boss','job',
+'dollar','degree''medicine','customers','nobody','light','fan','businessess']
 
 def send_allah_stuff(word, word_eol, userdata):
     my_msg = "%s are not doing Allah is doing" %(random.choice(world))
-    #channel = hexchat.get_info("")
+
     channel = hexchat.get_info("channel")
-    if len(word) < 2:
+    if len(word) < 2: #print message to a channel
         hexchat.command("MSG %s %s" %(channel, my_msg))
-    else if :
+    elif len(word) == 2: #print private message to a user
+        hexchat.command("MSG %s %s" %(word[1], my_msg))
+    elif len(word) == 3: #ddos version for boorish children
+        for x in range(0, 100):
             hexchat.command("MSG %s %s" %(word[1], my_msg))
 
 hexchat.hook_command("allah", send_allah_stuff)
