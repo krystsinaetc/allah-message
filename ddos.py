1f5ab0108fda106c8a41de7869326aff9fcf300f
Kristina <isitiriss@gmail.com>
2016-08-15 00:23:15 +0300

    last working version

--- ddos.py
+++ ddos.py
@@ -12,7 +12,7 @@ world = ['sun','moon','stars','planets','galaxies','oceans','mountains','trees',
 'dollar','degree''medicine','customers','nobody','light','fan','businessess']
 
 def send_stuff(word, word_eol, userdata):
-    allah_msg = "%s are not doing Allah is doing" %(random.choice(world))
+    my_msg = "%s are not doing Allah is doing" %(random.choice(world))
 
     channel = hexchat.get_info("channel")
     if len(word) < 2: #print message to a channel
@@ -20,7 +20,8 @@ def send_stuff(word, word_eol, userdata):
     elif len(word) == 2: #print private message to a user
         hexchat.command("MSG %s %s" %(word[1], my_msg))
     elif len(word) == 3: #ddos version for boorish children
-        for x in range(0, 100):
+        for x in range(0, 10):
+            my_msg = "%s are not doing Allah is doing" %(random.choice(world))
             hexchat.command("MSG %s %s" %(word[1], my_msg))
 
 hexchat.hook_command("ddos", send_stuff)
