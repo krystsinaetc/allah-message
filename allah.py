e0502c0fa461795ace22bff47af2faabb6e93cf5
Kristina <isitiriss@gmail.com>
2016-08-14 23:18:44 +0300

    working version

--- allah.py
+++ allah.py
@@ -10,13 +10,17 @@ __module_description__ = "Allah stuff for user"
 
 world = ['Allah1', 'Allah2', 'Allah3', 'Allah4', 'Allah5', 'Allah6', 'Allah7', 'Allah8', 'Allah9']
 
-def cmd_insult(word, word_eol, userdata):
+
+def send_allah_stuff(word, word_eol, userdata):
     my_msg = "%s are not doing Allah is doing" %(random.choice(world))
+    #channel = hexchat.get_info("")
     channel = hexchat.get_info("channel")
     if len(word) < 2:
         hexchat.command("MSG %s %s" %(channel, my_msg))
+    else if :
+            hexchat.command("MSG %s %s" %(word[1], my_msg))
 
-hexchat.hook_command("/allah", send_allah_stuff)
+hexchat.hook_command("allah", send_allah_stuff)
 
 
 
